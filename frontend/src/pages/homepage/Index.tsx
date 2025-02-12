0740cddf4ac15d2c6da69ecd02e64a52a91b2b0d
Davon Whitworth <63819806+DavonWhitworth@users.noreply.github.com>
2025-02-12 01:37:03 -0800

    Foundation (#3)
    * Built hierarchy, built pages, and set up configurations

--- frontend/src/pages/homepage/Index.tsx
+++ frontend/src/pages/homepage/Index.tsx
@@ -1,14 +1,15 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function HomePage() {
+    const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
+        <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,5 +32,3 @@ function App() {
     </>
   )
 }
-
-export default App

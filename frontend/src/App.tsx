689e83eb9648d2bd6b68c0a98ea647aaec20c08e
Davon <davonwhitworth@gmail.com>
2025-02-12 01:09:56 -0800

    hierarchy and pages setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+// Layout
+import NavBar from './components/layout/NavBar'
+
+// Pages
+import HomePage from './pages/homepage/Index'
+//import AboutUs from './pages/AboutUs'
+
+
+function App() {
+  
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App

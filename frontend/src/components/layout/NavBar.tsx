cc1d8b5f5f7395b6696daf8fc825281efba0f1fd
unknown <williamhyap@gmail.com>
2025-02-19 21:16:39 -0800

    Feat-Navbar-fix

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="nav">
+      <a href="/" className="site-title">
+      Sports-App
+      </a>
+      <ul>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+        <a href="/athletes">Athletes</a>
+        </li>
+        <li>
+        <a href="/contact">Services</a>
+        </li>
+        <li>
+        <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   )
 }

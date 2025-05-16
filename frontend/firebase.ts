78b907b9dd519c6682c515aefd7f204a672a1d2b
DerickT02 <derickt18@outlook.com>
2025-05-15 17:17:36 -0700

    ignore .env

---
+++ frontend/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRkkI_YzmTGisATzo95OC4nvvXQnwtHDU",
+  authDomain: "sports-app-7fc79.firebaseapp.com",
+  projectId: "sports-app-7fc79",
+  storageBucket: "sports-app-7fc79.firebasestorage.app",
+  messagingSenderId: "78165604371",
+  appId: "1:78165604371:web:9171017c65e13b3db067e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
